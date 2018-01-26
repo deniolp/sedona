b6aa0c5fad5cdef253f18d82ad3935b8af25ffbe
Denis Popov <deniolp@gmail.com>
2018-01-26 23:19:29 +0300

    Начал применять адаптивные сетки к главной странице

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 var mainNavToggle = document.querySelector(".main-nav__toggle");
 var listToggle = document.querySelector(".main-nav__list");
+var logoToggle = document.querySelector(".page-header__logo");
 
 mainNavToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -7,4 +8,5 @@ mainNavToggle.addEventListener("click", function(evt) {
   mainNavToggle.classList.toggle("main-nav__toggle");
   listToggle.classList.toggle("site-list--closed");
   listToggle.classList.toggle("site-list");
+  logoToggle.classList.toggle("page-header__logo--mobile");
 });
