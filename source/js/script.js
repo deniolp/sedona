359bfedc8ea0a144798ad34ee29ab1c1a8560424
Denis Popov <deniolp@gmail.com>
2018-01-29 11:22:26 +0300

    Сделал небольшие исправления для соответствия Б5 (было пару классов с модификатором при отстутствии класса без модификатора в этом же элементе). Также немного поправил js скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -5,8 +5,6 @@ var logoToggle = document.querySelector(".page-header__logo");
 mainNavToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
   mainNavToggle.classList.toggle("main-nav__toggle--closed");
-  mainNavToggle.classList.toggle("main-nav__toggle");
   listToggle.classList.toggle("site-list--closed");
-  listToggle.classList.toggle("site-list");
   logoToggle.classList.toggle("page-header__logo--mobile");
 });
