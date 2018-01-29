663d83b72be4bf5e90278d1cd9fb4e7322dccac6
Denis Popov <deniolp@gmail.com>
2018-01-29 22:56:50 +0300

    Переделал логику мобильного меню и скрипта, чтобы при отключенном js открывалось меню (nojs)

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,13 @@ var mainNavToggle = document.querySelector(".main-nav__toggle");
 var listToggle = document.querySelector(".main-nav__list");
 var logoToggle = document.querySelector(".page-header__logo");
 
+// В случае наличия JS лишние классы ниже убираются
+
+mainNavToggle.classList.remove("main-nav__toggle--nojs");
+mainNavToggle.classList.remove("main-nav__toggle--closed");
+listToggle.classList.add("site-list--closed");
+logoToggle.classList.remove("page-header__logo--mobile");
+
 // Реализация открытия и закрытия верхнего меню в мобильной версии
 
 mainNavToggle.addEventListener("click", function(evt) {
