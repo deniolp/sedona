9a9bcf1836dfa42d212f7fa740b0b48ac5d808e6
Denis Popov <deniolp@gmail.com>
2018-01-29 21:40:36 +0300

    Переделал скриптовый файлик для страницы формы

--- source/js/script-form.js
+++ source/js/script-form.js
@@ -1,3 +1,18 @@
+// Переменные для открытия и закрытия верхнего меню в мобильной версии
+
+var mainNavToggle = document.querySelector(".main-nav__toggle");
+var listToggle = document.querySelector(".main-nav__list");
+var logoToggle = document.querySelector(".page-header__logo");
+
+// Реализация открытия и закрытия верхнего меню в мобильной версии
+
+mainNavToggle.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mainNavToggle.classList.toggle("main-nav__toggle--closed");
+  listToggle.classList.toggle("site-list--closed");
+  logoToggle.classList.toggle("page-header__logo--mobile");
+});
+
 // Переменные для открытия модальных окон
 
 var popupSuc = document.querySelector(".pop-up--success");
